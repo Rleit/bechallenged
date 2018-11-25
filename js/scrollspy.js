646424d086553bcbb28851b0e4980b01f6e59d28
Rainer Leit <rleit@uclan.ac.uk>
2018-11-25 02:14:52 +0000

    Updates Form

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -3,42 +3,49 @@
 // ------------------------------
 
 function scrollNav() {
-    $('.nav-item a').click(function(){  
-      //Toggle Class
-      $(".active").removeClass("active");      
-      $(this).closest('li').addClass("active");
-      var theClass = $(this).attr("class");
-      $('.'+theClass).parent('li').addClass('active');
-      //Animate
-      $('html, body').stop().animate({
-          scrollTop: $( $(this).attr('href') ).offset().top - 160
-      }, 400);
-      return false;
+    $('.nav-item a').click(function () {
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.' + theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 160
+        }, 400);
+        return false;
     });
     $('.scrollTop a').scrollTop();
-  }
-  scrollNav();
+}
+scrollNav();
 
 
 
 
-  $(document).ready(function(){
 
-    var em = $('#mauticform_input_dailyprompts_email');
+var em = $('#mauticform_input_dailyprompts_email').val().trim();
+var esubmit = $('#mauticform_input_dailyprompts_submit');
 
-    function validateEmail(em) {
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        return emailReg.test( em );
-      }
-      
-    $("input").keyup(function(e){
-       var a=$('#mauticform_input_dailyprompts_email').val().trim();
-       if(!validateEmail(a)){
-       $('#mauticform_input_dailyprompts_submit').attr("disabled","true");
-       }
-       else{
-       $('#mauticform_input_dailyprompts_submit').removeAttr('disabled');
-       }
-    });
+function validateEmail(em) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test(em);
+}
+
+$("input").keyup(function (e) {
+    var a = em
+    if (!validateEmail(a)) {
+        esubmit.attr("disabled", "true");
+    } else {
+        esubmit.removeAttr('disabled');
+        esubmit.one('click', function () {
+            $("#thanks").modal();
+            esubmit.attr("disabled", "true");
+            $( "#emailabl" ).removeClass( "active" )
+            $('#mauticform_dailyprompts' ).each(function(){
+                this.reset();
+
+            });
+        });
+    }
 
 });
